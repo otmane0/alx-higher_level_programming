c077e5c5615297ea0cf699221260a3c47e5930cf
otmane0 <davenamir3@gmail.com>
2024-01-25 00:03:22 +0100

    test 2

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python
 for i in range(0, 100):
     if i == 99:
-        print("{:d}\n".format(i),end="")
+        print("{:02d}\n".format(i),end="")
     else:
-        print("{:d}".format(i), end= ", ")
+        print("{:02d}".format(i), end= ", ")
