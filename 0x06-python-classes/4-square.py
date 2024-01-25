37cec89bb873bf7eb9a9e4027f8f92f485dbe54e
otmane0 <davenamir3@gmail.com>
2024-01-25 03:40:00 +0100

    kd

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 
 class Square:
-
     def __init__(self, size=0):
         self.__size = size
 
@@ -15,7 +14,6 @@ class Square:
             raise TypeError("size must be an integer")
         elif value < 0:
             raise ValueError("size must be >= 0")
-
         self.__size = value
 
     def area(self):
