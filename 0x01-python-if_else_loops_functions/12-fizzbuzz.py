bc76ee7eeaf2390be35a818ae874ad276429a2ae
otmane0 <davenamir3@gmail.com>
2024-01-27 18:16:05 +0100

    test

---
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -0,0 +1,13 @@
+def fizzbuzz():
+    for i in range(1, 101):
+        fizz = (i % 3 == 0)
+        buzz = (i % 5 == 0)
+
+        if fizz and buzz:
+            print("FizzBuzz", end=" ")
+        elif fizz:
+            print("Fizz", end=" ")
+        elif buzz:
+            print("Buzz", end=" ")
+        else:
+            print("{:d}".format(i), end=" ")
