64acf94ed6478a403e02357e47ab7feb25050a4a
otmane0 <davenamir3@gmail.com>
2024-01-28 00:12:55 +0100

    test

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -3,8 +3,10 @@ if __name__ == "__main__":
     import sys
     arguments = sys.argv[1:]
 
+    if len(arguments) == 1:
+        print("{} argument:".format(len(arguments)))
+    else:
+        print("{} arguments:".format(len(arguments)))
 
-    print("{} argument:".format(len(arguments)))
-    i = 0
     for i, arr in enumerate(arguments):
         print("{:d}: {}".format(i + 1, arr))
