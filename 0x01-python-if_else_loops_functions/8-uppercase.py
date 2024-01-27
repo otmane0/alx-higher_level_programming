e6e05c3284bef1864603196800312805996f39ee
otmane0 <davenamir3@gmail.com>
2024-01-27 17:05:33 +0100

    test

---
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+def uppercase(s):
+    for char in s:
+        upper = ord(char)
+        print(upper)
+        """if 97 <= upper <= 122:
+            upper -= 32
+        print("{:c}".format(upper), end="")"""
+    print()
+
+"""def uppercase(s):
+    for charr in s:
+        upper = ord(charr)
+        if"""
