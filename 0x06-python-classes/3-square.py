b7d0593c374b7364c54aead88bb1114040bae308
otmane0 <davenamir3@gmail.com>
2024-02-04 11:06:43 +0100

    test

--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -4,7 +4,7 @@
 class Square:
     """Area of a square"""
 
-    def __init__(self, size=0):
+    def __init__(self, size = 0):
         if not isinstance(size, int):
             raise TypeError("size must be an integer")
         if size < 0:
