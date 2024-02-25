813249ab30d2d91ae10e1ecbd80706ccdadd3ef0
otmane0 <davenamir3@gmail.com>
2024-02-25 20:26:36 +0100

    test

--- 0x06-python-classes/100-singly_linked_list.py
+++ 0x06-python-classes/100-singly_linked_list.py
@@ -1,13 +1,14 @@
 #!/usr/bin/python3
 
 class Node:
-    def __init__(self, data, next_node=None) :
+    def __init__(self, data, next_node=None):
         self.__data = data
         self.__next_node = next_node
 
     @property
     def data(self):
         return self.__data
+
     @data.setter
     def data(self, value):
         if not isinstance(value, int):
@@ -18,10 +19,11 @@ class Node:
     @property
     def next_node(self):
         return self.__next_node
+
     @next_node.setter
     def next_node(self, value):
         if value is not None and not isinstance(value, Node):
-            raise TypeError ("next_node must be a Node object")
+            raise TypeError("next_node must be a Node object")
         else:
             self.__next_node = value
 
@@ -31,7 +33,11 @@ class SinglyLinkedList:
 
     def sorted_insert(self, value):
         new_node = Node(value)
-        if self.__head is None or self.__head.data >= value:
+
+        if self.__head is None:
+            new_node.next_node = None
+            self.__head = new_node
+        elif self.__head.data > value:
             new_node.next_node = self.__head
             self.__head = new_node
         else:
@@ -42,9 +48,9 @@ class SinglyLinkedList:
             current.next_node = new_node
 
     def __str__(self):
-        result = ""
+        val = []
         current = self.__head
         while current is not None:
-            result += str(current.data) + "\n"
+            val.append(str(current.data))
             current = current.next_node
-        return result
+        return '\n'.join(val)
