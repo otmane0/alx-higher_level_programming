4643050a1b519a513dcf106bfc9035d1caa0d663
otmane0 <davenamir3@gmail.com>
2024-02-25 14:57:38 +0100

    test

--- 0x06-python-classes/5-square.py
+++ 0x06-python-classes/5-square.py
@@ -1,28 +1,31 @@
 #!/usr/bin/python3
-"""Everything is object"""
+"""A module for Square"""
 
 class Square:
-    """Area of a square"""
-
-    def __init__(self, size = 0):
-        if not isinstance(size, int):
-            raise TypeError("size must be an integer")
-        if size < 0:
-            raise ValueError("size must be >= 0")
+    """
+    Access and update private attribute
+    """
+    def __init__(self, size=0) :
         self.__size = size
 
     def area(self):
-        """print def"""
+        """
+        Access and update private attribute
+        """
         return self.__size * self.__size
 
     @property
     def size(self):
-        """print def"""
+        """
+        Access and update private attribute
+        """
         return self.__size
 
     @size.setter
     def size(self, value):
-        """print def"""
+        """
+        Access and update private attribute
+        """
         if not isinstance(value, int):
             raise TypeError("size must be an integer")
         if value < 0:
@@ -30,12 +33,12 @@ class Square:
         self.__size = value
 
     def my_print(self):
-        """print def"""
         if self.__size == 0:
-            print()
-        else:
-            for i in range(self.__size):
-                for j in range(self.__size):
-                    print("#",end="")
-                print()
+            print("\n")
+        i=0
+        j=0
+        for i in range (self.__size):
+            for j in range (self.__size):
+                print("#", end="")
 
+            print("")
