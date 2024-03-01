10fe51e738e82e4d73f85ea0a42e5b0f8483578c
otmane0 <davenamir3@gmail.com>
2024-03-01 13:21:51 +0100

    test

--- 0x08-python-more_classes/7-rectangle.py
+++ 0x08-python-more_classes/7-rectangle.py
@@ -3,10 +3,10 @@
 
 
 class Rectangle:
-    """symbol of #"""
-    print_symbol = '#'
+
     """REpresent a rectangle"""
     number_of_instances = 0
+    print_symbol = '#'
 
     """Represent a rectangle."""
     def __init__(self, width=0, height=0):
