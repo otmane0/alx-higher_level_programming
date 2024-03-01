272d8ac5b9291876d9e0fc60e7a26c078a1cf5a3
otmane0 <davenamir3@gmail.com>
2024-03-01 21:05:03 +0100

    test

--- 0x08-python-more_classes/9-rectangle.py
+++ 0x08-python-more_classes/9-rectangle.py
@@ -58,7 +58,7 @@ class Rectangle:
         print("Bye rectangle...")
         Rectangle.number_of_instances -= 1
 
-    @classmethod
+    @staticmethod
     def bigger_or_equal(cls, rect_1, rect_2):
         """Return the bigger rectangle based on the area."""
         if not isinstance(rect_1, cls):
