98db8da49f2d25fae1f4a08e7eae6f90135b101d
otmane0 <davenamir3@gmail.com>
2024-03-05 16:27:43 +0100

    test

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python3
+"""file work"""
 
 def write_file(filename="", text=""):
+    """read file"""
+
 
     with open(filename, "w", encoding='utf-8') as File:
         File.write(text)
