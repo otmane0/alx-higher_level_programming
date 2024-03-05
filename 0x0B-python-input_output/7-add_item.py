0b462873cc09ed971f1ed591eab28eaedb34851b
otmane0 <davenamir3@gmail.com>
2024-03-05 18:36:25 +0100

    test

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -9,12 +9,12 @@ load_from = __import__('6-load_from_json_file').load_from_json_file
 arguments = sys.argv[1:]
 
 try:
-    old_data = load_from_json_file('add_item.json')
+    old_data = load_from('add_item.json')
 except FileNotFoundError:
     old_data = []
 
 old_data.extend(arguments)
-save_to_json_file('add_item.json', old_data)
+save_to('add_item.json', old_data)
 
 
 
