5b46a035d9ef016598f165a2226c7adeb3a12926
otmane0 <davenamir3@gmail.com>
2024-03-05 17:49:24 +0100

    test

--- 0x0B-python-input_output/6-load_from_json_file.py
+++ 0x0B-python-input_output/6-load_from_json_file.py
@@ -2,8 +2,8 @@
 """FIles"""
 
 import json
-"""json works"""
 
 def load_from_json_file(filename):
+    """json works"""
     with open(filename, "r", encoding='utf-8') as FIle:
         return json.load(filename)
