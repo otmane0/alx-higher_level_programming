bc070a63c4a6a61596567d2a4a734e52adc6fab3
otmane0 <davenamir3@gmail.com>
2024-03-05 11:10:58 +0100

    test

--- 0x0A-python-inheritance/3-is_kind_of_class.py
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -3,7 +3,4 @@
 
 def is_kind_of_class(obj, a_class):
     """function is_kind_of_class"""
-    if isinstance(obj, a_class) or isinstance(obj, object):
-        return True
-    else:
-        return False
+    return isinstance(obj, a_class)
