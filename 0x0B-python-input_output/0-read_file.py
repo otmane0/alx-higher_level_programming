05c41beee6409277d94698c0d27cca6f0cc5230f
otmane0 <davenamir3@gmail.com>
2024-03-05 16:15:23 +0100

    test

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -4,5 +4,5 @@
 def read_file(filename=""):
     """read file"""
 
-    with open(filename, encoding='utf-8') as FILE:
-        print(FILE.read())
+    with open(filename, "r", encoding='utf-8') as FILE:
+        print(FILE.read(), end="")
