f103da7b52f76c4be12545cea97da50189f0c7ad
otmane0 <davenamir3@gmail.com>
2024-03-05 13:44:51 +0100

    test

---
+++ 0x0A-python-inheritance/10-square.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+""" Module for Rectangle class """
+
+Rectangle = __import__('9-rectangle').Rectangle
+
+class Square(Rectangle):
+    """Square"""
+    def __init__(self, size):
+        """main func"""
+        self.integer_validator("size", size)
+        self.__size = size
+        super().__init__(size, size)
+
+    def area(self):
+        return self.__size * self.__size
+
+
+
