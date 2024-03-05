7283752e67214151bc8dd59cab9b55774d07f01b
otmane0 <davenamir3@gmail.com>
2024-03-05 11:23:52 +0100

    test

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -3,4 +3,4 @@
 
 def inherits_from(obj, a_class):
     """true of a class"""
-    return isinstance(obj, a_class) and type(obj) != a_class:
+    return isinstance(obj, a_class) and type(obj) != a_class
