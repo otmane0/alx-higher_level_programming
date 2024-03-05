2c99f8b13cffc6dc7ba23d8f464db08d595c95e4
otmane0 <davenamir3@gmail.com>
2024-03-05 16:12:45 +0100

    test

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -5,4 +5,4 @@ def read_file(filename=""):
     """read file"""
     filename = "UTF8"
     with open(filename, "r", encoding='utf-8') as FILE:
-        print(FILE.read(), end="")
+        print(FILE.read())
