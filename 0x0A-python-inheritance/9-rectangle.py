f9ba2a9758a2ab87cb8de4a7eeab293573651b42
otmane0 <davenamir3@gmail.com>
2024-03-05 13:19:28 +0100

    test

---
+++ 0x0A-python-inheritance/9-rectangle.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+""" Module for Rectangle class """
+
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
+
+class Rectangle(BaseGeometry):
+    """ Rectangle class inheriting from BaseGeometry """
+    def __init__(self, width, height):
+        """ Initialize Rectangle with width and height """
+        self.integer_validator("width", width)
+        self.integer_validator("height", height)
+        self.__width = width
+        self.__height = height
+
+    def erea(self):
+        """Return erea"""
+        erea = self.__width * self.__height
+
+
+    def __str__(self):
+        """str presentation"""
+        return ("[Rectangle]" + str(self.__width) + "/" + str(self.__height))
+
+
+
