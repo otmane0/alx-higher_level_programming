7e7a6d3d7ba7c5da547385341884bef75c5b6496
otmane0 <davenamir3@gmail.com>
2024-03-06 20:13:32 +0100

    test

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""Rectangle"""
+"""Rectangle module"""
 
 from models.base import Base
 
@@ -7,55 +7,49 @@ class Rectangle(Base):
     """Rectangle class"""
 
     def __init__(self, width, height, x=0, y=0, id=None):
-        """init func"""
-
+        """Initialize Rectangle instance"""
         super().__init__(id)
-
         self.width = width
         self.height = height
         self.x = x
         self.y = y
 
-
-
     @property
     def width(self):
-       """width"""
-       return self.__width
+        """Getter method for width"""
+        return self.__width
 
     @width.setter
     def width(self, value):
-        """setter"""
+        """Setter method for width"""
         self.__width = value
 
     @property
     def height(self):
-       """height"""
-       return self.__height
+        """Getter method for height"""
+        return self.__height
 
     @height.setter
     def height(self, value):
-        """setter"""
+        """Setter method for height"""
         self.__height = value
-
 
     @property
     def x(self):
-       """x"""
-       return self.__x
+        """Getter method for x"""
+        return self.__x
 
     @x.setter
     def x(self, value):
-        """"setter"""
+        """Setter method for x"""
         self.__x = value
 
     @property
     def y(self):
-       """y"""
-       return self.__y
+        """Getter method for y"""
+        return self.__y
 
     @y.setter
     def y(self, value):
-        """setter"""
+        """Setter method for y"""
         self.__y = value
-
