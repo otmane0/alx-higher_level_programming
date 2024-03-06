580172e11a1748393c7b5d2cce906ee3b41c938d
otmane0 <davenamir3@gmail.com>
2024-03-06 23:10:36 +0100

    test size

---
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""Square"""
+
+from models.rectangle import Rectangle
+
+class Square(Rectangle):
+    def __init__(self, size, x=0, y=0, id=None):
+        super().__init__(size, size, x, y, id)
+
+    def __str__(self):
+        """print"""
+        return "[{}] ({}) {}/{} - {}".format(type(self).__name__, self.id, self.__x, self.__y, self.size)
