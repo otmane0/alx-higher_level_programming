d860e8f69b8f63910478a7994770f578efcb3a50
otmane0 <davenamir3@gmail.com>
2024-03-07 10:35:53 +0100

    test

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -46,6 +46,6 @@ class Square(Rectangle):
     def to_dictionary(self):
         """To dict"""
 
-         return {'id': self.id, 'size': self.size, 'x': self.x, 'y': self.y}
+        return {'id': self.id, 'size': self.size, 'x': self.x, 'y': self.y}
 
 
