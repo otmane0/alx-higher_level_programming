f87d10e007853a68123cf4bbb1727324488e023c
otmane0 <davenamir3@gmail.com>
2024-03-07 10:55:02 +0100

    test

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 """Project"""
 
+import json
+
 class Base:
     """Class Base"""
 
@@ -13,3 +15,14 @@ class Base:
         else:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
+
+    @staticmethod
+    def to_json_string(list_dictionaries):
+        """using json"""
+        if list_dictionaries is None:
+            return "[]"
+        else:
+            return json.dumps(list_dictionaries)
+
+
+
