0ab4b3e0f0ffc14450b023fd7da2795b22b17048
otmane0 <davenamir3@gmail.com>
2024-03-07 20:45:18 +0100

    test

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -36,14 +36,15 @@ class Base:
         if json_string is None or json_string == "":
             return []
         return json.loads(json_string)
-
+    @classmethod
     def create(cls, **dictionary):
+        """new from None"""
         from models.rectangle import Rectangle
         from models.square import Square
         if cls is Rectangle:
             new = Rectangle(1, 1)
         elif cls is Square:
-            new = Square
+            new = Square(1)
         else:
             new = None
         new.update(**dictionary)
