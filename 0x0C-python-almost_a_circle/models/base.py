18a0768594a8dd2b4dbcbae1a970bc9b166830cd
otmane0 <davenamir3@gmail.com>
2024-03-07 12:26:05 +0100

    fix

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -24,5 +24,17 @@ class Base:
         else:
             return json.dumps(list_dictionaries)
 
+    @classmethod
+    def save_to_file(cls, list_objs):
+        """class method"""
+        if list_objs is not None:
+            list_objs = [o.to_dictionary() for o in list_objs]
+
+        with open("{}.json".format(cls.__name__), "w", encoding="utf-8") as FILE:
+            FILE.write(cls.to_json_string(list_objs))
+
+
+
+
 
 
