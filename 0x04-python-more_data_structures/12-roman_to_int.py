3229fd6606f9f7c44b175083b2f1b1aec0c07eb3
otmane0 <davenamir3@gmail.com>
2024-09-19 00:58:06 +0100

    kawtar

---
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -0,0 +1,27 @@
+def roman_to_int(roman_string):
+    if not isinstance(roman_string, str):
+        return 0
+
+    roman_values = {
+        'I': 1, 'V': 5, 'X': 10,
+        'L': 50, 'C': 100, 'D': 500,
+        'M': 1000
+    }
+
+    total = 0
+    prev_value = 0
+
+    for char in roman_string:
+        if char not in roman_values:
+            return 0
+
+        current_value = roman_values[char]
+
+        if current_value > prev_value:
+            total += current_value - 2 * prev_value
+        else:
+            total += current_value
+
+        prev_value = current_value
+
+    return total
