0f9fd379d14f88f479b529584fe120cdcf924d63
otmane0 <davenamir3@gmail.com>
2024-09-30 14:48:17 +0100

    delete by index

--- 0x03-python-data_structures/11-delete_at.py
+++ 0x03-python-data_structures/11-delete_at.py
@@ -1,19 +1,20 @@
+
+
 #!/usr/bin/python3
-"""Write a function that deletes the item at a specific position in a list.
-
-Prototype: def delete_at(my_list=[], idx=0):
-If idx is negative or out of range, nothing change (returns the same list)
-You are not allowed to use pop()
-You are not allowed to import any module"""
-
 def delete_at(my_list=[], idx=0):
     if idx < 0 or idx >= len(my_list):
-        return(my_list)
+        return my_list
 
     new_list = []
     for i in range(len(my_list)):
         if i != idx:
             new_list.append(my_list[i])
 
-    return (new_list)
+    my_list.clear()
+    for n in new_list:
+            my_list.append(n)
+
+    return my_list
+
+
 
