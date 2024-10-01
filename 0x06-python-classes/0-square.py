34f91acf6e01111cbdfbadd651010f728ac2b675
Othmane Kasmi <132145141+otmane0@users.noreply.github.com>
2024-10-01 15:35:36 +0100

    Delete 0x06-python-classes directory

--- 0x06-python-classes/0-square.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/python3
-"""A module for Square"""
-
-
-class Square:
-    """Represent a Square"""
-    pass
--- 0x06-python-classes/1-square.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/python3
-"""A module for Square"""
-
-class Square:
-    """Square"""
-    def __init__(self, size) :
-        self.__size = size
-
