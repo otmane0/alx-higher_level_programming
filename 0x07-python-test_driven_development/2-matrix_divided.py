18d907d82ca23fbffb8e35cb3e2fd1bba110342d
otmane0 <davenamir3@gmail.com>
2024-10-09 19:58:48 +0100

    2 matrix

---
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -0,0 +1,34 @@
+
+#!/usr/bin/python3
+def matrix_divided(matrix, div):
+    """Matrix with tests
+    """
+    if not isinstance(matrix, list) or not all(isinstance(row, list) for row in matrix):
+        raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
+
+    for a in matrix:
+        for b in a:
+            if not isinstance(b, (int, float)):
+                raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
+
+    len_mat = len(matrix[0])
+    for r in matrix:
+        if len(r) != len_mat:
+            raise TypeError("Each row of the matrix must have the same size")
+
+    if not isinstance(div, (int, float)):
+        raise TypeError("div must be a number")
+
+    if div == 0:
+        raise ZeroDivisionError("division by zero")
+
+
+    aray_div = []
+    for i in matrix:
+        row = []
+        for n in i:
+            row.append((round(n / div, 2)))
+        aray_div.append(row)
+
+    return aray_div
+
