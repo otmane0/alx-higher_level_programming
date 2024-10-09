c62967c293dbf8ed9716552b6453163c22c8c999
otmane0 <davenamir3@gmail.com>
2024-10-09 02:59:42 +0100

    0

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -1,11 +1,9 @@
-"""Write a function that adds 2 integers.
-
-Prototype: def add_integer(a, b=98):
-a and b must be integers or floats, otherwise raise a TypeError exception with the message a must be an integer or b must be an integer
-a and b must be first casted to integers if they are float
-Returns an integer: the addition of a and b
-You are not allowed to import any module"""
 #!/usr/bin/python3
 def add_integer(a, b=98):
-    """Test driven"""
-    return a + b
+    """Return the non-float.
+    """
+    if ((not isinstance(a, int) and not isinstance(a, float))):
+        raise TypeError("a must be an integer")
+    if ((not isinstance(b, int) and not isinstance(b, float))):
+        raise TypeError("b must be an integer")
+    return (int(a) + int(b))
