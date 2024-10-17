d5f757cca407ded2c304c0c70580932148b3db3c
otmane0 <davenamir3@gmail.com>
2024-10-17 22:28:28 +0100

    test

--- 0x0B-python-input_output/6-load_from_json_file.py
+++ 0x0B-python-input_output/6-load_from_json_file.py
@@ -1,7 +1,10 @@
 #!/usr/bin/python3
 """Define"""
+
 import json
+
 def load_from_json_file(filename):
     """Write"""
+
     with open(filename, "w", encoding="utf-8") as file:
         json.load(file)
