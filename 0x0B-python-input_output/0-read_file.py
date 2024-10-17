5d2aad763e3da4920150a6225b5d572c05a7e696
otmane0 <davenamir3@gmail.com>
2024-10-17 16:29:49 +0100

    fix error, i think

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -3,6 +3,5 @@
 def read_file(filename=""):
     """Read file"""
     with open(filename, "r", encoding="utf-8") as file:
-        for line in file:
-            print(line, end="")
+            print(file.read(), end="")
 
