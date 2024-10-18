9d84f642c2090a3983601c61a95c9db56ccc1224
otmane0 <davenamir3@gmail.com>
2024-10-18 21:04:18 +0100

    fix

--- 0x0B-python-input_output/10-student.py
+++ 0x0B-python-input_output/10-student.py
@@ -1,23 +1,32 @@
 #!/usr/bin/python3
+"""Defines a class Student."""
 
-"""We do return self7__dict__, to set key, value like a dictionry"""
 
 class Student:
-    """Class"""
+    """Represent a student."""
+
     def __init__(self, first_name, last_name, age):
-        """Init"""
+        """Initialize a new Student.
+
+        Args:
+            first_name (str): The first name of the student.
+            last_name (str): The last name of the student.
+            age (int): The age of the student.
+        """
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
 
     def to_json(self, attrs=None):
-        """To dict"""
-        if attrs == None:
-            return self.__dict__
-        else:
-            filtered_dict = []
-            for attr in attrs:
-                if hasattr(self, attr):
-                    filtered_dict[attr] = getattr(self, attr)
+        """Get a dictionary representation of the Student.
 
-            return filtered_dict.__dict__
+        If attrs is a list of strings, represents only those attributes
+        included in the list.
+
+        Args:
+            attrs (list): (Optional) The attributes to represent.
+        """
+        if (type(attrs) == list and
+                all(type(ele) == str for ele in attrs)):
+            return {k: getattr(self, k) for k in attrs if hasattr(self, k)}
+        return self.__dict__
