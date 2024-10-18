a84074ca3ce89f94306da019f01a3cfaf42f40c7
otmane0 <davenamir3@gmail.com>
2024-10-18 21:00:02 +0100

    student

---
+++ 0x0B-python-input_output/10-student.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+
+"""We do return self7__dict__, to set key, value like a dictionry"""
+
+class Student:
+    """Class"""
+    def __init__(self, first_name, last_name, age):
+        """Init"""
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """To dict"""
+        if attrs == None:
+            return self.__dict__
+        else:
+            filtered_dict = []
+            for attr in attrs:
+                if hasattr(self, attr):
+                    filtered_dict[attr] = getattr(self, attr)
+
+            return filtered_dict
