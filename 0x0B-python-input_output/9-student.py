ed625b8047088a9c7d39110a65bee12107b18bd9
otmane0 <davenamir3@gmail.com>
2024-10-18 19:15:50 +0100

    comments

--- 0x0B-python-input_output/9-student.py
+++ 0x0B-python-input_output/9-student.py
@@ -1,10 +1,13 @@
 #!/usr/bin/python3
-
+"""From objects"""
 class Student:
+    """Class"""
     def __init__(self, first_name, last_name, age):
+        """Init"""
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
 
     def to_json(self):
+        """To dict"""
         return self.__dict__
