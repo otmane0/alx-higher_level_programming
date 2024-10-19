8c8ab34ce63c5849cb64e8186aa18efb086351c9
otmane0 <davenamir3@gmail.com>
2024-10-19 18:21:08 +0100

    s

--- 0x0B-python-input_output/11-student.py
+++ 0x0B-python-input_output/11-student.py
@@ -1,9 +1,11 @@
 #!/usr/bin/python3
-"""Defines a class Student."""
+"""Defines a class Student.
+"""
 
 
 class Student:
-    """Represent a student."""
+    """Represent a student.
+    """
 
     def __init__(self, first_name, last_name, age):
         """Initialize a new Student.
