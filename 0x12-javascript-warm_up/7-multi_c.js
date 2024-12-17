8a21f4d0a73f3f460a89787567367a65db8d62ab
otmane0 <davenamir3@gmail.com>
2024-12-17 22:27:25 +0100

    try to fix output again

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -3,8 +3,10 @@
 const args = process.argv.slice(2);
 const num = parseInt(args[0]);
 
-if (isNaN(num) || num <= 0) {
+if (isNaN(num)) {
   console.log('Missing number of occurrences');
 } else {
-  for (let i = 0; i < num; i++) { console.log('C is fun'); }
+  for (let i = 0; i < num; i++) {
+    console.log('C is fun');
+  }
 }
