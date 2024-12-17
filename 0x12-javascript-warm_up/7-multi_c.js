f0f4ede8d12dba3cff741a3bc4348d380367adec
otmane0 <davenamir3@gmail.com>
2024-12-17 22:25:35 +0100

    converted

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -1,9 +1,10 @@
 #!/usr/bin/node
 
 const args = process.argv.slice(2);
+const num = parseInt(args[0]);
 
-if (isNaN(args[0]) || args[0] === undefined || args[0] <= 0) {
+if (isNaN(num) || num <= 0) {
   console.log('Missing number of occurrences');
 } else {
-  for (let i = 0; i < args[0]; i++) { console.log('C is fun'); }
+  for (let i = 0; i < num; i++) { console.log('C is fun'); }
 }
