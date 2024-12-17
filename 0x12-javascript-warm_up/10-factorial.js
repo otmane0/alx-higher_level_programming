47c2f69c26fda474b4a44a9666ce10b9154b2acf
otmane0 <davenamir3@gmail.com>
2024-12-17 23:05:12 +0100

    recursion

---
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -0,0 +1,14 @@
+#!/usr/bin/node
+
+function factorial (n) {
+  if (isNaN(n) || n === 0) {
+    return 1;
+  }
+
+  return n * factorial(n - 1);
+}
+
+const args = process.argv.slice(2);
+const num = parseInt(args[0]);
+
+console.log(factorial(num));
