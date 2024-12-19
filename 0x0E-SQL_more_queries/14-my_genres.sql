ef770b7e98dd6fa2869d7de70001923f7aecd950
otmane0 <davenamir3@gmail.com>
2024-12-19 21:24:59 +0100

    3 tables, 2 times join

--- 0x0E-SQL_more_queries/14-my_genres.sql
+++ 0x0E-SQL_more_queries/14-my_genres.sql
@@ -12,8 +12,9 @@ ON
 JOIN 
     tv_shows 
 ON 
-    tv_shows.id = tv_show_genres.show_id
+     tv_show_genres.show_id = tv_shows.id 
 WHERE 
     tv_shows.title = 'Dexter'
 ORDER BY 
     tv_genres.name ASC;
+
