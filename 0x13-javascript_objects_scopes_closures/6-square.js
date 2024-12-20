857bb55a2e81799fe501456fa543cb16bdf75a8c
otmane0 <davenamir3@gmail.com>
2024-12-20 23:44:24 +0100

    ge

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -6,17 +6,19 @@ Second class, Inhertance
 const Rectangle = require('./4-rectangle');
 
 class Square extends Rectangle {
-  constructor (size) {
-    super(size, size);
+  constructor(size) {
+    super(size, size); // Call the parent class's constructor
   }
 
-  charPrint (c) {
-    if (c === 'C') {
+  charPrint(c) {
+    if (c) {
+      // Temporarily modify the parent's `print()` logic by overriding its behavior
       for (let i = 0; i < this.height; i++) {
         console.log(c.repeat(this.width));
       }
     } else {
-      this.print();
+      // Use the parent class's `print()` method for default behavior
+      super.print();
     }
   }
 }
