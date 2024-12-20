2eef4267ecbef605906b7b06b06cc724f9dcb6e4
otmane0 <davenamir3@gmail.com>
2024-12-21 00:45:46 +0100

    length

--- 0x13-javascript_objects_scopes_closures/7-occurrences.js
+++ 0x13-javascript_objects_scopes_closures/7-occurrences.js
@@ -1,10 +1,10 @@
 #!/usr/bin/node
 
 exports.nbOccurences = function (list, searchElement) {
-  let n = 0;
-  for (let i = 0; i < list.lenght; i++) {
+  let n = 0; let i = 0;
+  for (i; i < list.length; i++) {
     if (list[i] === searchElement) {
-      n += 1;
+      n++;
     }
   }
   return n;
