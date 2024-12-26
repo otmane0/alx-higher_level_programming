86d284147bc2eaf1cfda58f446071736b29ad7c3
otmane0 <davenamir3@gmail.com>
2024-12-26 14:38:07 +0100

    link with database

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -1,23 +1,20 @@
 #!/usr/bin/python3
 
-"""sqlalchemy"""
+"""Sqlalchemy"""
 
 from sqlalchemy import Column, Integer, String, create_engine
 from sqlalchemy.orm import declarative_base
 
-url = "mysql+mysqlconnector://root:@localhost:3306/teest?charset=utf8mb4"
 
-
-engine = create_engine(url)
 
 Base = declarative_base()
 class State(Base):
+    """
+    First class
+    """
     __tablename__ = "states"
 
     id = Column(Integer ,primary_key=True, autoincrement=True, nullable=False)
     name = Column(String(128), nullable=False)
-
-
-
 
 
