2b6bc6ddcd137e5549deb6367fbc3038e4b5d128
otmane0 <davenamir3@gmail.com>
2024-12-26 23:50:04 +0100

    update

--- 0x0F-python-object_relational_mapping/13-model_state_delete_a.py
+++ 0x0F-python-object_relational_mapping/13-model_state_delete_a.py
@@ -1,32 +1,19 @@
-
 #!/usr/bin/python3
-
-"""Sqlalchemy"""
+""" Get a state
+"""
+from sys import argv
 from model_state import Base, State
-from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
-import sys
+
+from sqlalchemy import (create_engine)
 
 if __name__ == "__main__":
-
-    mysql_username = sys.argv[1]
-    mysql_password = sys.argv[2]
-    database_name = sys.argv[3]
-
-    url = f"mysql+mysqldb://{mysql_username}:{mysql_password}@localhost:3306/{database_name}?charset=utf8mb4"
-
-
-    engine = create_engine(url)
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}\
+'.format(argv[1], argv[2], argv[3]), pool_pre_ping=True)
     Base.metadata.create_all(engine)
-
-    Session = sessionmaker(bind=engine)
-    session = Session()
-    states = session.query(State).filter(State.name.like('%a%')).all()
-
-
-
-    for state in states:
-        session.delete(state)
-
+    session = sessionmaker(bind=engine)()
+    all_a = session.query(State).filter(State.name.like('%a%\
+')).all()
+    for one in all_a:
+        session.delete(one)
     session.commit()
-
