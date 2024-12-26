ae69d93ddc33e4ae7b997ae3f8750f91b6586389
otmane0 <davenamir3@gmail.com>
2024-12-26 18:46:46 +0100

    filters

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -1,25 +1,26 @@
 #!/usr/bin/python3
+"""Mysqldb lib"""
 
 if __name__ == "__main__":
     import MySQLdb
     from sys import argv
 
-db = MySQLdb.connect(
-    host='localhost',
-    user=argv[1],
-    password=argv[2],
-    db=argv[3],
-    port=3306
-)
+    db = MySQLdb.connect(
+        host='localhost',
+        user=argv[1],
+        password=argv[2],
+        db=argv[3],
+        port=3306
+    )
 
-cursor = db.cursor()
-
-
-cursor.execute("SELECT id, name FROM states ORDER BY id ASC")
-result = cursor.fetchall()
-for x in result:
-    print(x)
+    cursor = db.cursor()
 
 
-cursor.close()
-db.close()
+    cursor.execute("SELECT id, name FROM states ORDER BY id ASC")
+    result = cursor.fetchall()
+    for x in result:
+        print(x)
+
+
+    cursor.close()
+    db.close()
--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -1,27 +1,28 @@
 #!/usr/bin/python3
+"""Mysqldb lib"""
 
 if __name__ == "__main__":
     import MySQLdb
     from sys import argv
 
-db = MySQLdb.connect(
-    host='localhost',
-    user=argv[1],
-    passwd=argv[2],
-    db=argv[3],
-    port=3306
-)
+    db = MySQLdb.connect(
+        host='localhost',
+        user=argv[1],
+        passwd=argv[2],
+        db=argv[3],
+        port=3306
+    )
 
-cursor = db.cursor()
+    cursor = db.cursor()
 
 
-query = f"SELECT id, name FROM states WHERE name = '{argv[4]}' ORDER BY id ASC"
+    query = f"SELECT id, name FROM states WHERE name = '{argv[4]}' ORDER BY id ASC"
 
-cursor.execute(query)
+    cursor.execute(query)
 
-result = cursor.fetchall()
-for row in result:
-    print(row)
+    result = cursor.fetchall()
+    for row in result:
+        print(row)
 
-cursor.close()
-db.close()
+    cursor.close()
+    db.close()
