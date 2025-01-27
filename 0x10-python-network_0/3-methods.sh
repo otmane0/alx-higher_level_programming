a5d364ee8684c644dadca0438cf5943ad8de142f
otmane0 <davenamir3@gmail.com>
2025-01-27 17:01:35 +0100

    HTTP Requests

--- 0x10-python-network_0/3-methods.sh
+++ 0x10-python-network_0/3-methods.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 #When you use -d, you are telling the command "here is a specific value or piece of data that I want you to use in this operation.
-curl -sI | grep -i 'Allow:' | cut -d ' ' -f2- | tr -d '\r' $@ 
+curl -sI $@ | grep -i 'Allow:' | cut -d ' ' -f2-
