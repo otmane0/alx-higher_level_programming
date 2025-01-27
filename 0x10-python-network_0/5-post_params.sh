a068abd273c34937c246aadb753c98e3c37ea634
otmane0 <davenamir3@gmail.com>
2025-01-27 15:33:16 +0100

    ff

--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 #display the length of the conetent
-curl -s -X POST -d "email=test@gmail.com&subject=I will always be here for PLD" $@
+curl -s -X POST $@ -d "email=test@gmail.com&subject=I will always be here for PLD" 
