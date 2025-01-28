0c48a673e170c6cece3ee918d251ade7e9562aba
otmane0 <davenamir3@gmail.com>
2025-01-29 00:01:06 +0100

    get data from request

---
+++ 0x11-python-network_1/8-json_api.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""fetch specific attr from header of URL"""
+
+from sys import argv
+import requests
+
+if __name__ == '__main__':
+
+    url = "http://0.0.0.0:5000/search_user"
+    q = argv[1]
+    if not argv[1]:
+        q=""
+
+    data = {'q': q}
+
+    try:
+        response = requests.post(url, data=data).json()
+        if not response:
+            print("No result") #empty JSon
+        else:
+            if "id" in response and "name" in response:
+                    print(f"[{response['id']}] {response['name']}")
+
+    except requests.exceptions.JSONDecodeError:
+        print("Not a valid JSON")  # Response is not valid JSON
+
+
+
