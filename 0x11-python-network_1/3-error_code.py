971c36c20fabf6ba95141f218f1ed37294673457
otmane0 <davenamir3@gmail.com>
2025-01-28 18:44:04 +0100

    error code: 404

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -1,11 +1,14 @@
 #!/usr/bin/python3
 """manage error exception"""
+
 from sys import argv
 from urllib.request import urlopen
 from urllib.error import HTTPError
 
 if __name__ == '__main__':
+
     url = argv[1]
+
     try:
         with urlopen(url) as response:         # Send the request to the URL and read the response
             body = response.read().decode('utf-8')
