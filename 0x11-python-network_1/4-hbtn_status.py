1514aad2136a0c916429b42624044fc306601dc9
otmane0 <davenamir3@gmail.com>
2025-01-28 18:57:38 +0100

    fetch url with requests lib

---
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""Fetch url with request"""
+
+from sys import argv
+import requests
+
+if __name__ == '__main__':
+
+   url = "https://alx-intranet.hbtn.io/status"
+
+   body = requests.get(url)
+   print("Body response:")
+   print("\t- type:", type(body.text))
+   print("\t- content:", body.text)
