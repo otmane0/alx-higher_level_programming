087e7207a37ac118d429161fb068a323370a54f5
otmane0 <davenamir3@gmail.com>
2025-01-28 17:18:51 +0100

    access to the header

---
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+#Get specific key, from a URL header
+from sys import argv
+
+from urllib.request import urlopen
+
+url = argv[1]
+
+with urlopen(url) as response:
+    headers = response.info()
+
+    x_request_id = headers.get("X-Request-Id")
+    print(x_request_id)
