4362d468b129f52772e2879ae71d35972234621c
otmane0 <davenamir3@gmail.com>
2025-01-28 17:45:12 +0100

    ff

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -3,9 +3,10 @@
 from sys import argv
 from urllib.request import urlopen
 
-url = argv[1]
+if __name__ == '__main__':
+    url = argv[1]
 
-with urlopen(url) as response:
-    headers = response.info()
-    x_request_id = headers.get("X-Request-Id")
-print(x_request_id)
+    with urlopen(url) as response:
+        headers = response.info()
+        x_request_id = headers.get("X-Request-Id")
+    print(x_request_id)
