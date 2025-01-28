78f6096c9bd5851561d074689c10ba6876cb6ea1
otmane0 <davenamir3@gmail.com>
2025-01-28 19:00:32 +0100

    fetch URL with requests

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -10,4 +10,4 @@ if __name__ == '__main__':
    body = requests.get(url)
    print("Body response:")
    print("\t- type:", type(body.text))
-   print("\t- content:", body.content)
+   print("\t- content:", body.text)
